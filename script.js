c6bbfc6976cc8bbfd5b47d6e3a987d08f8ae175b
Sahil Moktan <moktanshail9@gmail.com>
2023-06-09 13:18:19 +0545

    javascript logic working normal calculations

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let string = "";
 
+//queryselectorAll selects .button class actions
 let buttons = document.querySelectorAll(".button");
 
+//make array from buttton and use foreach
+//addEventListener listens click  on array function, e is event listen/recorded
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target);
-    string = string + e.target.innerHTML;
-    document.querySelector("input").value = string;
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string; // putting outputString in inputfield of html
+    } else if (e.target.innerHTML == "=") {
+      string = " ";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = " ";
+      document.querySelector("input").value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML; //on upper string(let) adding self as more string occor from event
+      document.querySelector("input").value = string;
+    }
   });
 });
